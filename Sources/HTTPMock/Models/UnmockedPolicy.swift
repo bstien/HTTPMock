009d092220c6aee55f56039d7b99f071c61cf6b3
Bastian Stien <bastian.stien@gmail.com>
2025-09-13 13:48:21 +0200

    UnmockedPolicy: Add .mock(MockResponse)

--- Sources/HTTPMock/Models/UnmockedPolicy.swift
+++ Sources/HTTPMock/Models/UnmockedPolicy.swift
@@ -5,6 +5,9 @@ public enum UnmockedPolicy {
     /// Return a hardcoded "Not found" message, along with HTTP status 404.
     case notFound
 
+    /// Return a user-defined mocked response.
+    case mock(MockResponse)
+
     /// Let the request pass through to the internet.
     /// Useful for integration tests where you want to mock some responses, but let others hit actual network.
     case passthrough
