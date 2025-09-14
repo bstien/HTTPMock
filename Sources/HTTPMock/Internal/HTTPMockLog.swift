0fe9b2c3af0e4cde757d0850ce634798b530ab91
Bastian Stien <bastian.stien@gmail.com>
2025-09-14 12:39:31 +0200

    Fix debug logging

--- Sources/HTTPMock/Internal/HTTPMockLog.swift
+++ Sources/HTTPMock/Internal/HTTPMockLog.swift
@@ -1,37 +1,48 @@
+import Foundation
 import os
 
 public enum HTTPMockLog {
     public static var logger = Logger(subsystem: "httpmock", category: "HTTPMock")
     public static var isEnabled = true
-    public static var level: Level = .info
+    public static var level: Level = .trace
 
-    public static func error(_ message: String) {
+    public static func error(_ message: @autoclosure () -> String) {
         if should(.error) {
-            logger.error("[HTTPMock] \(message, privacy: .public)")
+            let message = message()
+            logger.error("[HTTPMock][\(level.description)] \(message, privacy: .public)")
+            printToConsole("⛔️ \(message)", level: .error)
         }
     }
 
-    public static func warning(_ message: String) {
+    public static func warning(_ message: @autoclosure () -> String) {
         if should(.warning) {
-            logger.warning("[HTTPMock] \(message, privacy: .public)")
+            let message = message()
+            logger.warning("[HTTPMock][\(level.description)] \(message, privacy: .public)")
+            printToConsole("⚠️ \(message)", level: .warning)
         }
     }
 
-    public static func info(_ message: String) {
+    public static func info(_ message: @autoclosure () -> String) {
         if should(.info) {
-            logger.info("[HTTPMock] \(message, privacy: .public)")
+            let message = message()
+            logger.info("[HTTPMock][\(level.description)] \(message, privacy: .public)")
+            printToConsole(message, level: .info)
         }
     }
 
-    public static func debug(_ message: String) {
+    public static func debug(_ message: @autoclosure () -> String) {
         if should(.debug) {
-            logger.debug("[HTTPMock] \(message, privacy: .public)")
+            let message = message()
+            logger.debug("[HTTPMock][\(level.description)] \(message, privacy: .public)")
+            printToConsole(message, level: .debug)
         }
     }
 
-    public static func trace(_ message: String) {
+    public static func trace(_ message: @autoclosure () -> String) {
         if should(.trace) {
-            logger.log("[HTTPMock] \(message, privacy: .public)")
+            let message = message()
+            logger.log("[HTTPMock][\(level.description)] \(message, privacy: .public)")
+            printToConsole(message, level: .trace)
         }
     }
 
@@ -40,6 +51,21 @@ public enum HTTPMockLog {
     @inline(__always)
     private static func should(_ l: Level) -> Bool {
         isEnabled && l.rawValue <= level.rawValue
+    }
+
+    @inline(__always)
+    private static var isXcodePreview: Bool {
+        ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+    }
+
+    private static func printToConsole(_ message: String, level: Level) {
+        #if DEBUG
+        Swift.print("[HTTPMock][\(level.description)] \(message)")
+        #else
+        if isXcodePreview {
+            Swift.print("[HTTPMock][\(level.description)] \(message)")
+        }
+        #endif
     }
 }
 
@@ -50,5 +76,15 @@ extension HTTPMockLog {
         case info
         case debug
         case trace
+
+        var description: String {
+            switch self {
+            case .error: "ERROR"
+            case .warning: "WARNING"
+            case .info: "INFO"
+            case .debug: "DEBUG"
+            case .trace: "TRACE"
+            }
+        }
     }
 }
